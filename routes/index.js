950d1d82173c74574f603984172659d1826e6124
chopmac <chopmack84@gmail.com>
2026-07-15 20:02:45 -0700

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,14 @@ const { login, createUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
 const { NOT_FOUND } = require("../utils/errors");
 
-
 router.post("/signin", login);
 router.post("/signup", createUser);
 
+router.use("/items", itemRouter);
 
 router.use(auth);
 
-
 router.use("/users", userRouter);
-router.use("/items", itemRouter);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
